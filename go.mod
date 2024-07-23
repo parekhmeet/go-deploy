e0cb7b617c277e8939d7ee55531a2e081a879bcd
parekhmeet <parekhmeet17@gmail.com>
2024-07-23 19:11:52 +0530

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/parekhmeet/go-web-app
+
+go 1.22.5
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/home.html")
+}
+
+func aboutPage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/about.html")
+}
+
+func contactPage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/contact.html")
+}
+
+func main() {
+
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/about", aboutPage)
+	http.HandleFunc("/contact", contactPage)
+
+	err := http.ListenAndServe("0.0.0.0:8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
